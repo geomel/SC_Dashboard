ad59f14685180421f137e7034072956012044174
geomel <geomel@gmail.com>
2022-03-19 11:14:00 +0200

    added contracts page and imports

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import Home from "./pages/home/Home";
 import Login from "./pages/login/Login";
 import List from "./pages/list/List";
 import Single from "./pages/single/Single";
+import Contracts from "./pages/contracts/Contracts";
 import New from "./pages/new/New";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { productInputs, userInputs } from "./formSource";
